deb8c5759e247e8504709e6847304bb2a8fcde2b
Toshikazu Ban <35644224+banto354@users.noreply.github.com>
2023-07-07 06:22:22 +0000

    [Add]ユーザー情報編集機能

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -4,8 +4,24 @@ class Public::UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(current_user)
+    else
+      render :edit
+    end
   end
 
   def check
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :username, :birthdate, :email, :introduction)
   end
 end
