5988ed094b1adb10db5fe14d365869b2007aedfc
Toshikazu Ban <35644224+banto354@users.noreply.github.com>
2023-07-07 10:50:18 +0000

    [Add]Flash message

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
   
   root to:'public/homes#top'
+  
+  # ユーザー用
+  devise_for :users, controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+  }
+
+# 管理者用
+  devise_for :admin, controllers: {
+  sessions: "admin/sessions"
+  }
+
   get '/about' => 'public/homes#about'
   scope module: :public do
     patch 'users/withdrawal' => "users#withdrawal", as: 'withdrawal_user'
@@ -10,15 +22,6 @@ Rails.application.routes.draw do
   
   namespace :admin do
   end
-# ユーザー用
-  devise_for :users, controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
-}
 
-# 管理者用
-  devise_for :admin, controllers: {
-  sessions: "admin/sessions"
-}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
