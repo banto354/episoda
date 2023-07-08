933c6ca7b41b1d0a3421b8a180453ab7df55c399
Toshikazu Ban <35644224+banto354@users.noreply.github.com>
2023-07-08 09:10:00 +0000

    [Add]Commentモデル、コントローラー追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_08_050116) do
+ActiveRecord::Schema.define(version: 2023_07_07_233449) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -50,23 +50,6 @@ ActiveRecord::Schema.define(version: 2023_07_08_050116) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
-  end
-
-  create_table "episodes", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.string "title", null: false
-    t.text "content", null: false
-    t.integer "visibility", default: 0, null: false
-    t.integer "group_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "favourites", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "episode_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "posts", force: :cascade do |t|
