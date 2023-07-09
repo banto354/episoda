6a0101aae93b801f5dc9b6f9f930cddfef7677b4
Toshikazu Ban <35644224+banto354@users.noreply.github.com>
2023-07-09 06:01:41 +0000

    [Add]ヘッダーロゴ

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,14 @@ class User < ApplicationRecord
   has_many :favourites, dependent: :destroy
   has_many :comments, dependent: :destroy
   
+  validates :name, presence: true, length: { minimum: 2, maximum: 20 }
+    # 半角英数字のみを許可する正規表現（英字は小文字のみ）
+  ALPHANUMERIC_REGEX = /\A[a-z0-9]+\z/
+  validates :username, presence: true, length: { minimum: 2, maximum: 20 }, uniqueness: true, format: { with: ALPHANUMERIC_REGEX }
+  validates :introduction, length: { maximum: 400 }
+  validates :email, uniqueness: true
+  
+  # ユーザー画像サイズ変換
   def get_profile_image(width, height)
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
