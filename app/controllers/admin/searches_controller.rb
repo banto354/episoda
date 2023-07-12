ed8e763a7232d1a36302ed3b61dacf4775951f66
Toshikazu Ban <35644224+banto354@users.noreply.github.com>
2023-07-12 10:35:56 +0000

    [Update]admin用検索窓

--- app/controllers/admin/searches_controller.rb
+++ app/controllers/admin/searches_controller.rb
@@ -1,4 +1,9 @@
 class Admin::SearchesController < ApplicationController
   def index
+    @query = params[:query]
+    @users = User.where("name LIKE ?", "%#{@query}%")
+    # ユーザー検索結果から検索者を除外
+    @users = @users.where.not(id: current_user.id) unless current_user.nil?
+    @episodes = Episode.where("title LIKE ?", "%#{@query}%")
   end
 end
