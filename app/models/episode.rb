f4a543503e861fb5403a2d5ec486ed1437a88823
Toshikazu Ban <35644224+banto354@users.noreply.github.com>
2023-07-15 13:20:33 +0000

    [Add]ハッシュタグ

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -3,6 +3,8 @@ class Episode < ApplicationRecord
   belongs_to :user
   has_many :favourites, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :taggings
+  has_many :tags, through: :taggings  
   
   enum visibility: { to_public: 0, to_group: 1, to_myself: 2 }
   
@@ -13,7 +15,7 @@ class Episode < ApplicationRecord
   after_create do
     episode = Episode.find_by(id: self.id)
     tags  = self.content.scan(/[#＃][\w\p{Han}ぁ-ヶｦ-ﾟー]+/)
-    tags.uniq.map do |tag|
+    tags.uniq.each do |tag|
       #ハッシュタグは先頭の'#'を外した上で保存
       hashtag = Tag.find_or_create_by(name: tag.downcase.delete('#'))
       episode.tags << hashtag
