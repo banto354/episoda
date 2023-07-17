fd721bfc157bd7e40ea88dc60b6ca5fc11a3bf24
Toshikazu Ban <35644224+banto354@users.noreply.github.com>
2023-07-17 05:46:27 +0000

    [Update]検索結果画面レイアウト調整(ページネーション前)

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,10 +1,10 @@
 class Public::SearchesController < ApplicationController
   def index
     @query = params[:query]
-    @users = User.where("name LIKE ?", "%#{@query}%")
+    @users = User.where("name LIKE ?", "%#{@query}%").page(params[:page]).per(5)
     # ユーザー検索結果から検索者を
     @users = @users.where.not(id: current_user.id) unless current_user.nil?
-    @episodes = Episode.where("title LIKE ?", "%#{@query}%")
+    @episodes = Episode.where("title LIKE ?", "%#{@query}%").page(params[:page]).per(5)
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,3 @@
 module ApplicationHelper
-  def episode_favourite_icon(episode, user)
-    count = episode.favourites.count
-    if episode.favourited_by?(user)
-      link_to(episode_favourite_path(episode), method: :delete, remote: true) do
-        "<i class='fa-solid fa-heart'></i> #{count}".html_safe
-      end
-    else
-      link_to(episode_favourite_path(episode), method: :post, remote: true) do
-        "<i class='fa-regular fa-heart'></i> #{count}".html_safe
-      end
-    end
-  end
+
 end
