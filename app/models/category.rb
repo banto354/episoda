5f2db0fba2dcf6b8fe9ca3ff12cf82a841b97eac
Toshikazu Ban <35644224+banto354@users.noreply.github.com>
2023-07-19 07:43:41 +0000

    [Update]カテゴリ付修正中

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,10 @@
 class Category < ApplicationRecord
+  has_many :episodes, through: :category_relations
+  has_many :category_relation, dependent: :destroy
+
+  with_options presence: true do
+    validates :name
+  end
+
   enum parent: { emotion: 0, genre: 1, year: 2 }
 end
--- app/models/category_relation.rb
+++ app/models/category_relation.rb
@@ -1,4 +1,9 @@
 class CategoryRelation < ApplicationRecord
-  belongs_to :user, dependent: :destroy
+  belongs_to :episode, dependent: :destroy
   belongs_to :category, dependent: :destroy
+  
+  with_options presence: true do
+    validates :episode_id
+    validates :category_id
+  end
 end
