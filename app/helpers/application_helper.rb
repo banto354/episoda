a8b358966ab1c40a3b9a29f385088575bb675e7d
Toshikazu Ban <35644224+banto354@users.noreply.github.com>
2023-07-29 01:48:44 +0000

    [Fix]フラッシュメッセージ等のエラー

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,12 @@
 module ApplicationHelper
-
+  def bootstrap_alert(obj)
+    if obj == "notice"
+      alert = "info"
+    elsif obj == "success"
+      alert == "success"
+    elsif obj == "alert"
+      alert = "danger"
+    end
+    return alert
+  end
 end
