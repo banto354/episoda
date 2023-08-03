4688e8d01bfee857731162362248730f95987fd4
Toshikazu Ban <35644224+banto354@users.noreply.github.com>
2023-08-03 00:37:56 +0000

    [Add]deviceのskip

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,13 @@ Rails.application.routes.draw do
   get '/about' => 'public/homes#about'
 
   # ユーザー用
-  devise_for :users, controllers: {
+  devise_for :users, skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
   }
 
 # 管理者用
-  devise_for :admin, controllers: {
+  devise_for :admin,  skip: [:registrations, :passwords], controllers: {
   sessions: "admin/sessions",
   }
 
