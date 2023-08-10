b5d81dfcaedafaf006b185f3b5e551a1d1e6d872
Toshikazu Ban <35644224+banto354@users.noreply.github.com>
2023-08-10 20:49:59 +0000

    [Update]comment削除用modalテンプレート統一

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -4,7 +4,7 @@ class Admin::CommentsController < ApplicationController
     comment.destroy
     @comments = Comment.where(episode_id: params[:episode_id])
     @episode = Episode.find(params[:episode_id])
-    flash[:success] = "コメントを削除しました"
+    flash[:notice] = "コメントを削除しました"
     redirect_to admin_episode_path(comment.episode)
   end
 end
