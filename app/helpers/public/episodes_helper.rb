fda542243ba599b3f366d6cbb75a8ae61b75a386
Toshikazu Ban <35644224+banto354@users.noreply.github.com>
2023-08-28 05:06:25 +0000

    [Update]エピソード新規投稿のplaceholderを変更

--- app/helpers/public/episodes_helper.rb
+++ app/helpers/public/episodes_helper.rb
@@ -1,6 +1,14 @@
 module Public::EpisodesHelper
+
   def render_with_tags(content)
     # タグにリンクを作成
     simple_format(content.gsub(/[#＃][\w\p{Han}ぁ-ヶｦ-ﾟー]+/){|word| link_to word, "/tag/#{word.delete("#")}"}.html_safe)
-  end  
+  end
+
+  def explanation
+    <<-"EOS".strip_heredoc
+      本文
+      (#〇〇〇 と書くことでハッシュタグを生成できます)
+    EOS
+  end
 end
